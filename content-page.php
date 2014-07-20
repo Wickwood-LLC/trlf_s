f5d2f493594042649a8810eecbbee160457f29a8
iMac27SteveW <webmaster@wickwoodonline.com>
2014-07-20 10:46:12 -0400

    Initial commit of TRLF_s theme created with Sass, Compass, Bourbon and CodeKit app.

---
+++ content-page.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template used for displaying page content in page.php
+ *
+ * @package TRLF_s Custom Theme
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'trlf_s' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+	<footer class="entry-footer">
+		<?php edit_post_link( __( 'Edit', 'trlf_s' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
