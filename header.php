5eb62181233660c6697de08b6b6e55acb428e6a8
iMac27SteveW <webmaster@wickwoodonline.com>
2014-09-10 19:26:19 -0400

    Make responsive header

--- header.php
+++ header.php
@@ -22,30 +22,38 @@
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'trlf_s' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<a class="logo-container" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-				<div class="logo"></div>
-			</a>
-			<div class="site-title-container">
-				<h1 class="site-title-line1"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">The</a></h1>
-				<h1 class="site-title-line2"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Randall Law Firm</a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	<header id="masthead" class="masthead" role="banner">
+		<div class="masthead-inner">
+			<div class="site-header-container">
+				<div class="site-branding">
+					<div class="site-branding-container">
+						<a class="logo-container" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<div class="logo"></div>
+						</a>
+						<div class="site-title-container">
+							<h1 class="site-title-line1"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">The</a></h1>
+							<h1 class="site-title-line2"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Randall Law Firm</a></h1>
+							<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+						</div>
+						<div class="site-contact-container">
+							<h3><span class="site-header-address">1777 Western Ave. Albany, NY 12203</span><span class="site-header-phone">Phone: 518-869-1205</span><span class="site-header-fax">Fax: 518-452-2145</span></h3>
+							<div class="site-header-attorney-container">
+								<h3 class="site-header-attorney">Denise McCarthy Randall</h3> 
+								<h3 class="site-header-attorney">Robert G. Randall</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="header-image-container">
+					<div class="header-image"></div>
+				</div>
 			</div>
-			<div class="site-contact-container">
-				<h3><span class="site-header-address">1777 Western Ave. Albany, NY 12203</span><span class="site-header-phone">Phone: 518-869-1205</span><span class="site-header-fax">Fax: 518-452-2145</span></h3>
-				<h3 class="site-header-attorney">Denise McCarthy Randall</h3> 
-				<h3 class="site-header-attorney">Robert G. Randall</h3>
-			</div>
-			<div class="header-image-container">
-				<div class="header-image"></div>
-			</div>
-		</div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'trlf_s' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle"><?php _e( 'Primary Menu', 'trlf_s' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- #site-navigation -->
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
