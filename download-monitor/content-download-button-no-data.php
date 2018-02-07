1826ee6b4a48376ee3275bd0cc3744db9efb3c63
iMac27SteveW <webmaster@wickwoodonline.com>
2018-02-07 15:35:22 -0500

    Fix typo in target attributes.

--- download-monitor/content-download-button-no-data.php
+++ download-monitor/content-download-button-no-data.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** @var DLM_Download $dlm_download */
 ?>
-<p><a class="aligncenter download-button" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow" target="_blank>
+<p><a class="aligncenter download-button" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow" target="_blank">
 		<?php printf( __( 'Download &ldquo;%s&rdquo;', 'download-monitor' ), $dlm_download->get_title() ); ?>
 		<small><?php echo $dlm_download->get_version()->get_filename(); ?></small>
 	</a></p>
