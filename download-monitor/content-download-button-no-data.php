3cab1a1c2d8e4029cfa3d3d381b29087fa45be15
iMac27SteveW <webmaster@wickwoodonline.com>
2018-02-07 15:26:52 -0500

    Added template for button with no data about number of downloads or filesize

---
+++ download-monitor/content-download-button-no-data.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Download button no count no filesize
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+/** @var DLM_Download $dlm_download */
+?>
+<p><a class="aligncenter download-button" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow" target="_blank>
+		<?php printf( __( 'Download &ldquo;%s&rdquo;', 'download-monitor' ), $dlm_download->get_title() ); ?>
+		<small><?php echo $dlm_download->get_version()->get_filename(); ?></small>
+	</a></p>
