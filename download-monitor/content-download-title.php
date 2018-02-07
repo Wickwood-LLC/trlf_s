5b9a11d58d83a4f4aa67a5cee0ca52600636f0f1
iMac27SteveW <webmaster@wickwoodonline.com>
2018-02-07 15:20:54 -0500

    Add target="_blank to content-download-title.php file

--- download-monitor/content-download-title.php
+++ download-monitor/content-download-title.php
@@ -11,6 +11,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <a class="download-link" title="<?php if ( $dlm_download->get_version()->has_version_number() ) {
 	printf( __( 'Version %s', 'download-monitor' ), $dlm_download->get_version()->get_version_number() );
-} ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow">
+} ?>" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow" target="_blank>
 	<?php $dlm_download->the_title(); ?>
 </a>
