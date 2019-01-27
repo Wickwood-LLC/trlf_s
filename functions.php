a9f67812ed579aa7cf9a98364106ccb67d200491
iMac27SteveW <webmaster@wickwoodonline.com>
2019-01-27 15:51:12 -0500

    Corrected where comment started to comment out Published date

--- functions.php
+++ functions.php
@@ -425,8 +425,8 @@ function theme_get_metadata_icons($icons = '', $class = '') {
     for ($i = 0; $i < count($icons); $i++) {
         $icon = $icons[$i];
         switch ($icon) {
-            case 'date':
-            /*$result[] = '<span class="art-postdateicon">' . sprintf(__('<span class="%1$s">Published</span> %2$s', THEME_NS),
+            /*case 'date':
+            $result[] = '<span class="art-postdateicon">' . sprintf(__('<span class="%1$s">Published</span> %2$s', THEME_NS),
                                 'date',
                                 sprintf( '<span class="entry-date updated" title="%1$s">%2$s</span>',
                                     esc_attr( get_the_time() ),
